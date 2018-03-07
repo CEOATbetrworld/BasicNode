069dc28a217d4c387e3355f2add9f7e3c6d9711d
DHRUV KUMAR <dkiphones620@gmail.com>
2018-03-07 20:27:59 +0530

    one more error method on response for better error reporting

--- http core/index.js
+++ http core/index.js
@@ -7,12 +7,15 @@ http.get(url, (response) => {
     let buffer = ''
     response.on('data', (chunk) => {
         c++
-       // console.log(chunk.toString('utf-8'))
-       buffer += chunk
+        // console.log(chunk.toString('utf-8'))
+        buffer += chunk
     })
     response.on('end', () => {
         console.log(`response has ended with ${c} chunk(s)`)
         console.log(`Data stored in buffer = ${buffer}`)
+    })
+    response.on('error', (error) => {
+        console.error(`Got error : ${error.message} `)
     })
 }).on('error', (error) => {
     console.error(`Got error : ${error.message} `)
