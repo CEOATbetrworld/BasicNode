365a9c1250226a8f6104fa1076fd233b811c720d
DHRUV KUMAR <dkiphones620@gmail.com>
2018-03-07 20:21:31 +0530

    using buffer

--- http core/index.js
+++ http core/index.js
@@ -1,16 +1,19 @@
 const http = require('http')
 
-const url = 'http://www.bctfibernet.com/'
+const url = 'http://www.nodeprogram.com/'
 
 http.get(url, (response) => {
     let c = 0
+    let buffer = ''
     response.on('data', (chunk) => {
         c++
-        console.log(chunk.toString('utf-8'))
+       // console.log(chunk.toString('utf-8'))
+       buffer += chunk
     })
     response.on('end', () => {
         console.log(`response has ended with ${c} chunk(s)`)
+        console.log(`Data stored in buffer = ${buffer}`)
     })
 }).on('error', (error) => {
-    console.error(`Got error : ${error.message}`)
+    console.error(`Got error : ${error.message} `)
 })
